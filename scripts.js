6d0cd93427d56cd60e42e62654d62148e0d66e54
Robert Lee <bobbydwise1@hotmail.com>
2019-09-03 21:50:20 -0700

    Finish UI and bussiness logic.

--- scripts.js
+++ scripts.js
@@ -25,32 +25,37 @@ class Queue{
   dequeue(yourElement){
     while ( this.data.stack.length > 0 ) {
       if ( yourElement == this.data.stack[0] ) {
-        setTimeout(function(){}, 1000)
+        console.log('Delete ', this.data.stack[0])
         this.data.stack.shift();
         break;
       } else {
-        setTimeout(function(){}, 1000)
         this.temp.stack.unshift(this.data.stack.shift())
       }
+      console.log('this.data.stack: ', this.data.stack)
+      console.log('this.temp.stack: ', this.temp.stack)
+      console.log('-----------------------------------')
     }
     while ( this.temp.stack.length > 0 ) {
-      setTimeout(function(){}, 1000)
       this.data.stack.unshift(this.temp.stack.shift())
+      console.log('this.data.stack: ', this.data.stack)
+      console.log('this.temp.stack: ', this.temp.stack)
+      console.log('-----------------------------------')
     }
     return this.data.stack
   }
 }
 
-let test0 = new Queue()
-test0.enqueue(0)
-test0.enqueue(1)
-test0.enqueue(2)
-test0.enqueue(3)
-test0.enqueue(4)
-test0.enqueue(5)
+// let test0 = new Queue()
+// test0.enqueue(0)
+// test0.enqueue(1)
+// test0.enqueue(2)
+// test0.enqueue(3)
+// test0.enqueue(4)
+// test0.enqueue(5)
+// test0.dequeue(3)
 // console.log(test0)
-test0.dequeue(3)
-console.log(test0)
+
+
 
 $(document).ready(function() {
   let input1;
@@ -61,7 +66,7 @@ $(document).ready(function() {
     console.log(input1)
     user.enqueue(input1)
     $('#output-1').text(JSON.stringify(user.data.stack))
-    $('#output-2').text(JSON.stringify(user.data.temp))
+    //$('#output-2').text(JSON.stringify(user.data.temp))
   });
   $('#form2').submit(function(){
     event.preventDefault()
@@ -69,6 +74,6 @@ $(document).ready(function() {
     console.log(input2)
     user.dequeue(input2)
     $('#output-1').text(JSON.stringify(user.data.stack))
-    $('#output-2').text(JSON.stringify(user.data.temp))
+    //$('#output-2').text(JSON.stringify(user.data.temp))
   });
 });
